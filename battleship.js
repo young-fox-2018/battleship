e2bf7c73b0e6b302589b0fc507f6e98a9003ac4f
Desy Armariena <desy.armariena@gmail.com>
2018-11-01 18:59:53 +0700

    Check result function

--- battleship.js
+++ battleship.js
@@ -38,14 +38,12 @@ function printBoard(){
     // print the ship
     for (let i = 0; i < SHIPS.length; i++) {
         // cek apakah kosong
-        let direction = horizontalVertical();
-        // console.log('arah', direction);
+        let direction = horizontalVertical();        
         let empty = checkEmpty(board, SHIPS[i], direction);
         
         while (empty === false) {
             SHIPS[i].coordinate[0] = randomCoordinate();
-            SHIPS[i].coordinate[1] = randomCoordinate();
-            // console.log(SHIPS[i].coordinate, '=================')
+            SHIPS[i].coordinate[1] = randomCoordinate();            
             empty = checkEmpty(board, SHIPS[i], direction);
         }
 
@@ -53,28 +51,37 @@ function printBoard(){
         if (empty === true) {
             board = insertShips(board,SHIPS[i], direction);
         }
-        // if (empty) {
-        //     for (let j = 0; j < SHIPS[i].size*2; j+=2) {
-        //         board = insertShips(board,SHIPS[i]);
-        //         // board[SHIPS[i].coordinate[0]][SHIPS[i].coordinate[1]+j] = ` ${SHIPS[i].graphic} `;
-        //     }  
-        // } else {
-        //     SHIPS[i].coordinate[0] = randomCoordinate();
-        //     SHIPS[i].coordinate[1] = randomCoordinate();
-        // }
-        // board[SHIPS[i].coordinate[0]][SHIPS[i].coordinate[1]] = ` ${SHIPS[i].graphic} `;
+    }
+
+    // print the bomb
+    for (let i = 0; i < args.length; i++) {
+        let coordinateBombs = [args[i][1]*2, args[i][0]];
+        let alphabet = 'ABCDEFGHIJ';
+        let convertAlphabet = [2,4,6,8,10,12,14,16,18,20];        
+
+        // convert column coordinate
+        for (let j = 0; j < alphabet.length; j++) {
+            if (coordinateBombs[1] === alphabet[j]) {
+                coordinateBombs[1] = convertAlphabet[j];
+            }            
+        }
+
+        board = insertBombs(board, coordinateBombs);
+        // console.log(coordinateBombs);
     }
 
 
     return board.join('\n');
 }
 
+function insertBombs(board, coordinate){
+    board[coordinate[0]][coordinate[1]] = '!!!';
+    return board;
+}
+
 function checkEmpty(board, ship, direction) {
 
     for (let i = 0; i < ship.size*2; i+=2) {
-        // console.log('kapal: ', ship.graphic, direction, ship.coordinate[0], ship.coordinate[1]+i)        
-        // console.log('horzontal: ', ship.coordinate[1] + i)
-        // console.log(board[ship.coordinate[0]][ship.coordinate[1]], ship.coordinate[0], ship.coordinate[1])
         if (direction === 'horizontal') {
             if (board[ship.coordinate[0]][ship.coordinate[1]+i] === undefined || board[ship.coordinate[0]][ship.coordinate[1]+i] !== `   ` || ship.coordinate[1]+(ship.size*2) >= 20) {
                 return false;
@@ -120,6 +127,28 @@ function horizontalVertical(){
     return 'vertical';
 }
 
+function checkResult(){
+    var countA = 0;
+    var countB = 0;
+    var countC = 0;
+    var countD = 0;
+
+    for (let i = 0; i < BOARD.length; i++) {                
+        if(BOARD[i] === 'A'){
+            countA++;
+        } else if(BOARD[i] === 'B'){
+            countB++;
+        } else if(BOARD[i] === 'C'){
+            countC++;
+        } else if(BOARD[i] === 'D'){
+            countD++;
+        }        
+    }
+
+    // TODO: Refactor this 
+    return `Kapal ${SHIPS[0].name} tertembak: ${SHIPS[0].size-countA+1}, sisa ${countA-1}\nKapal ${SHIPS[1].name} tertembak: ${SHIPS[1].size-countB+1}, sisa ${countB-1}\nKapal ${SHIPS[2].name} tertembak: ${SHIPS[2].size-countC+1}, sisa ${countC-1}\nKapal ${SHIPS[3].name} tertembak: ${SHIPS[3].size-countD+1}, sisa ${countD-1}`;
+}
+
 const SHIPS = [
     { name: 'Aircraft carrier', size: 5, graphic: 'A', coordinate: [randomCoordinate(), randomCoordinate()]},
     { name: 'Battleship', size: 4, graphic: 'B', coordinate: [randomCoordinate(), randomCoordinate()]},
@@ -127,8 +156,7 @@ const SHIPS = [
     { name: 'Destroyer', size: 2, graphic: 'D', coordinate: [randomCoordinate(), randomCoordinate()]}
 ]
 
-const BOARD = printBoard();
 const args = process.argv.slice(2);
+const BOARD = printBoard();
 console.log(BOARD);
-console.log(args);
-// console.log(SHIPS);
+console.log(checkResult());
