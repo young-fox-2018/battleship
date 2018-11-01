13f0945069b40c490bcba9f0ddab985635ee3d31
abednegolubis <lubisabednego@gmail.com>
2018-11-01 13:26:28 +0700

    submit release  0

--- battleship.js
+++ battleship.js
@@ -1,1 +1,20 @@
-// Your code here
+function generateBoard(dimension) {
+    var baris = ['','A','B','C','D','E','F','G','H','I','J'] 
+    for (let i = 0; i <= dimension; i++) {
+        var board = []
+        for (let j = 0; j <= dimension; j++) {
+            if(i === 0 & j=== 0) {
+                board.push(' x ')
+            }else if(i === 0 & j!==0){
+                board.push(` ${j} `)
+            }else if(j === 0 && i !== 0){
+                board.push(` ${baris[i]} `)
+            } else {
+               board.push('   ')
+           }         
+        } 
+        console.log(board.join('|'))
+    }
+}
+
+console.log(generateBoard(10))
