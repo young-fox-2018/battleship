71ba8db8f98739c78e08870f9f6eb8084e16e70b
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-01 18:21:06 +0700

    push dlu

--- battleship.js
+++ battleship.js
@@ -1,1 +1,193 @@
-// Your code here
+
+//      A   B   C   D   E   F   G   H   I   J   0
+//    +---------------------------------------+ 0
+//  1 |   |   |   |   |   |   |   |   |   |   | 1
+//    |---|---|---|---|---|---|---|---|---|---| 2
+//  2 |   |   |   |   |   |   |   |   |   |   | 3
+//    |---|---|---|---|---|---|---|---|---|---| 4
+//  3 |   |   |   |   |   |   |   |   |   |   | 5
+//    |---|---|---|---|---|---|---|---|---|---| 6
+//  4 |   |   |   |   |   |   |   |   |   |   | 7
+//    |---|---|---|---|---|---|---|---|---|---| 8
+//  5 |   |   |   |   |   |   |   |   |   |   | 9
+//    |---|---|---|---|---|---|---|---|---|---| 10 
+//  6 |   |   |   |   |   |   |   |   |   |   | 11
+//    |---|---|---|---|---|---|---|---|---|---| 12 
+//  7 |   |   |   |   |   |   |   |   |   |   | 13
+//    |---|---|---|---|---|---|---|---|---|---| 14
+//  8 |   |   |   |   |   |   |   |   |   |   | 15
+//    |---|---|---|---|---|---|---|---|---|---| 16
+//  9 |   |   |   |   |   |   |   |   |   |   | 17
+//    |---|---|---|---|---|---|---|---|---|---| 18
+// 10 |   |   |   |   |   |   |   |   |   |   | 19
+//    +---------------------------------------+ 20
+//    0123456789012345678901234567890123456789012
+
+function printBoard(num) {
+  var result = []
+  var batassamping = (num * 2)
+  var batasbawah = (num * 4)
+
+  for (let i = 0 ; i <= batassamping; i++){
+    var arr = []
+
+    for (let j = 0 ; j <= batasbawah; j++){
+      if (i === 0|| i === batassamping) {
+        if(i === 0 && j === 0 || i === 0 && j === batasbawah || i === batassamping && j === 0 || i === batassamping && j === batasbawah) {
+          arr.push('+')
+        }else{
+          arr.push('-')
+        }
+      } else if( i === 0 ){
+
+      }
+       else if ( j % 4 === 0){
+        arr.push('|')
+      } else if(j % 4 !== 0){
+        if(i % 2 !== 0){
+          arr.push(' ')
+        }else {
+          arr.push('-')
+        }
+      }
+    }
+    // result.push(arr.join(''))
+    result.push(arr)
+  }
+  // return result.join('\n')
+  return result
+}
+
+// console.log(printBoard(10))
+
+let temp = []
+for ( let i = 0; i < 10; i++ ) {
+  temp.push([])
+  for (let j = 0 ; j < 10 ; j++ ){
+    temp[i].push(' ')
+  }
+}
+// console.log(temp)
+
+const ships = [
+  {name: 'Aircraft carrier' ,size: 5, pos: [],print:"A"},
+  {name: 'Battleship' ,size: 4, pos: [] , print:'B'},
+  {name: 'Cruiser' ,size: 3, pos: [], print:'C'},
+  {name: 'Destroyer' ,size: 2, pos: [],print:'D'},
+]
+function printShip() {
+
+  var arah = ['vertikal' , 'horizontal']
+  debugger
+
+  for (let i = 0 ; i < ships.length ; i++){
+    iawal = random()
+    jawal = random()
+    let value = arah[Math.round(Math.random())]
+    let flag = true
+    var arr = []
+    var cek = false
+
+    while(cek === false){
+
+      for(let j = 0 ; j < ships[i].size ; j++){
+        if(value === 'vertikal'){
+          if (checkCoordinate(iawal+j, jawal) === false) {
+            flag = false
+          }else {
+            // console.log('masuk sini ver')
+            arr.push([iawal+j,jawal])
+          }
+        } else if ( value === 'horizontal'){
+          if (checkCoordinate(iawal, jawal+j) === false) {
+            flag = false
+          } else {
+            // console.log('masuk sini ver')
+
+            arr.push([iawal,jawal+j])
+          }
+        }
+      }
+    }
+    // console.log(arr)
+    if(flag === true) {
+      ships[i].pos = arr
+      for(let j = 0 ; j < ships[i].size ; j++){
+        if(value === 'vertikal'){
+          temp[iawal+j][jawal] = ships[i].print
+        } else if ( value === 'horizontal'){
+          temp[iawal][jawal+j] = ships[i].print
+        }
+      }
+    }
+  }
+  return temp
+}
+
+console.log(printShip())
+
+function checkCoordinate(row, col) {
+   //apakah coordinate ini sudah di pakai di kapal yg lain ?
+   // apakah coordinate ini melebihi board ?
+  var condition = false
+  // console.log('aaaaa', row, col)
+  if(temp[row] === undefined){
+    return condition
+  }
+  if(temp[row][col] === ' '){
+    condition = true
+  }
+   return condition
+}
+
+
+function random() {
+  var angka = Math.floor(Math.random() * 10)
+  return angka
+}
+
+var bom = process.argv.slice(2)
+
+// console.log(bom)
+var tampung = []
+for (let i = 0 ; i < bom.length ; i++) {
+  tampung.push([bom[i][0] , bom[i][1]] )
+}
+// console.log(tampung)
+
+function bandingBom() {
+  var kena = false
+  for (let i = 0 ; i < ships.length ; i++) {
+    for(let j = 0 ;j< ships[i].pos.length;j++ ){
+      // console.log(ships[i].pos[j])
+      for(let a = 0 ; a < tampung.length ; a++) {
+          if(ships[i].pos[j][0] == tampung[a][0] && ships[i].pos[j][1] == tampung[a][1]){
+            kena = true
+          }
+        }
+    }
+  }
+  return kena
+}
+
+// function showShip() {
+//   // ships
+//   let board = temp 
+
+//   // looping board 
+//     // looping ships 
+//       // bandingkan koordinat ship dengan koordinat board 
+//   for ( let i = 0 ; i < board.length ; i ++) {
+//     for (let j = 0 ; j < board[i].length ; j++) {
+//       for (let a = 0 ; a < ships.length ; a++) {
+//         for (let b = 0 ; b < ships[a].pos.length ; b++) {
+//           if(ships[a].pos[b][0] === i &&ships[a].pos[b][1] === j){
+//             board[i][j] = ships[a].print
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return board
+// }
+// console.log(showShip())
